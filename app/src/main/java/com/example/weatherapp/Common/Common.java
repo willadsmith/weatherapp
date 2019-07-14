2eb99b26aa9666e4c0fb35c50a3f50212bf369c4
vladimir rabtsun <vladimir.rabtsun@gmail.com>
2019-07-15 00:42:34 +0600

    Add JSON Model and components

--- app/src/main/java/com/example/weatherapp/Common/Common.java
+++ app/src/main/java/com/example/weatherapp/Common/Common.java
@@ -11,13 +11,13 @@ public class Common {
     public static String API_LINK = "http://api.openweathermap.org/data/2.5/weather";
 
     @NonNull
-    public static String apiRequest(String lat, String lng){
+    public static String apiRequest(String lat, String lng) {
         StringBuilder sb = new StringBuilder(API_LINK);
         sb.append(String.format("?lat=%s&lon=%s&APPID=%s&units=metric", lat,lng, API_KEY));
         return sb.toString();
     }
 
-    public static String unixTimeStampToDateTime(double unixTimeStamp){
+    public static String unixTimeStampToDateTime(double unixTimeStamp) {
         DateFormat dateFormat = new SimpleDateFormat("HH:mm");
         Date date = new Date();
         date.setTime((long)unixTimeStamp*1000);
@@ -26,5 +26,11 @@ public class Common {
 
     public static String getImage(String icon) {
         return String.format("http://openweathermap.org/img/w/$s.png", icon);
+    }
+
+    public static String getDateNow() {
+        DateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy HH:mm");
+        Date date = new Date();
+        return dateFormat.format(date);
     }
 }
